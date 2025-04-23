f86a9029306f06c988e11947a66e972c149a0041
youngmin <dudals2963@tukrorea.ac.kr>
2025-04-24 00:26:30 +0900

    카테고리 컴포넌트 구현 완료

--- src/base-ui/list/CategoryTag/index.jsx
+++ src/base-ui/list/CategoryTag/index.jsx
@@ -1,3 +1,34 @@
 import styled from "styled-components";
 
-export default function name() {}
+const chipColorMap = {
+  생활: 10,
+  "쇼핑/뷰티": 20,
+  "의료/건강": 30,
+  식비: 50,
+  교통: 60,
+  "문화/여가": 100,
+  미분류: 110,
+  월급: 20,
+  기타수입: 10,
+  용돈: 40,
+};
+
+export default function CategoryTag({ classification }) {
+  return <Tag $classification={classification}>{classification}</Tag>;
+}
+
+const Tag = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 92px;
+  height: 56px;
+  font-size: ${({ theme }) => theme.font.size.xs};
+  font-weight: ${({ theme }) => theme.font.weight.light};
+  line-height: 24px;
+  font-family: ${({ theme }) => theme.font.sans};
+  background-color: ${({ theme, $classification }) =>
+    theme.color.token.chip[chipColorMap[$classification]]};
+  color: ${({ theme }) => theme.color.token.text.rev};
+  text-align: center;
+`;
