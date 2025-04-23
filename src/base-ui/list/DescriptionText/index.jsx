b5a5cdd5e3b435fb08fc57b4327614cdd7cd618e
youngmin <dudals2963@tukrorea.ac.kr>
2025-04-24 00:56:48 +0900

    description항목 컴포넌트 구현 완료

--- src/base-ui/list/DescriptionText/index.jsx
+++ src/base-ui/list/DescriptionText/index.jsx
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
-export default function name() {}
+export default function DescriptionText({ description }) {
+  return <Text>{description}</Text>;
+}
+
+const Text = styled.div`
+  /* width: 400px;
+  height: 24px; */
+  font-family: ${({ theme }) => theme.font.sans};
+  font-weight: 300;
+  font-size: ${({ theme }) => theme.font.size.sm};
+  line-height: ${({ theme }) => theme.font.lineHeight.sm};
+  color: ${({ theme }) => theme.color.token.text.default};
+`;
